f97d93c45f81fe10677fd1ead13136900003a079
Axel Utech <axel.utech@gmail.com>
2015-11-22 11:39:34 +0100

    improve scripts

--- create_all_data.sh
+++ create_all_data.sh
@@ -7,4 +7,6 @@ docker build --tag=openseamap/mediawiki-docker ./mediawiki-docker && \
 docker run --name=mediawiki-data -v /data \
     openseamap/mediawiki-docker echo "data-only container for mediawiki"
 
-docker run --name=mysql-data -v /var/lib/mysql mysql echo "data-only container for mysql"
+docker build --tag=openseamap/mysql-docker ./mysql-docker && \
+docker run --name=mysql-data -v /var/lib/mysql -e MYSQL_USER=mysql  \
+    openseamap/mysql-docker echo "data-only container for mysql"
